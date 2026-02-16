58da42710ea9f0fd168bba9fd32d778f51a013fd
VaibhavFanClub <vaibhavfanclub2@gmail.com>
2026-02-16 15:54:04 +0530

    set b q4 assignment 4

---
+++ OptionMenu/app/java/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.optionmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem i){
+        int itemId = i.getItemId();
+        switch (itemId){
+            case R.id.o1:
+                Intent w = new Intent(MainActivity.this, Withdraw.class);
+                startActivity(w);
+                return true;
+            case R.id.o2:
+                Intent d = new Intent(MainActivity.this, Deposit.class);
+                startActivity(d);
+                return true;
+            case R.id.o3:
+                Intent b = new Intent(MainActivity.this, Balance.class);
+                startActivity(b);
+                return true;
+            default:
+                return super.onOptionsItemSelected(i);
+        }
+    }
+}
