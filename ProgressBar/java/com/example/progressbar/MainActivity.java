4838947675efc5bfcc583688b541b4419671cc7b
VaibhavFanClub <vaibhavfanclub2@gmail.com>
2026-03-16 15:19:12 +0530

    progress bar init

---
+++ ProgressBar/java/com/example/progressbar/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.progressbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar p1, p2;
+    Button b;
+    int p = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        p1 = findViewById(R.id.p1);
+        p2 = findViewById(R.id.p2);
+        b = findViewById(R.id.b);
+
+        b.setOnClickListener(v -> {
+            p += 10;
+            p2.setProgress(p);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
